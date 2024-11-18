456f217c7eef49087d2b64017249a9a06700b7c2
maarten442 <maartenrottier@live.nl>
2024-11-18 08:35:11 +0100

    created the create hash table function

--- ht.c
+++ ht.c
@@ -6,9 +6,24 @@ typedef struct {
 } ht_entry;
 
 typedef struct {
-    ht_entry* ht;
+    ht_entry* ht; // points to array of ht_entries
     size_t capacity;
     size_t length;
 } hash_table;
 
 // size_t: unsigned: best practice to use for memory or array objects. 
+
+#define INITIAL_CAPACITY 16;
+// Create pointer to a hashtable
+
+hash_table* ht_create(void) {
+
+    hash_table* table = malloc(sizeof(hash_table));
+    table->capacity = INITIAL_CAPACITY;
+    table->length = 0;
+
+    ht_entry* entry_array = calloc(table->capacity, sizeof(ht_entry));
+    table->ht = entry_array;
+
+    return table;
+}
